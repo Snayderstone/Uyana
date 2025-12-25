8ef32d3c0ef5dcb6b6bbde440f5f32f3130ddf40
snayderstone <luis10828steam@gmail.com>
2025-12-25 09:04:09 -0500

    delete page tests

--- src/routes/test-map/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { ProjectService } from '$lib/services/project.service';
-
-export const load = async () => {
-  const data = await ProjectService.getProjectsForMap();
-  return { data };
-};
